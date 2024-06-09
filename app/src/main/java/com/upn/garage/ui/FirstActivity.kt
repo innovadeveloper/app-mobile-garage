fd70c2fab3ce8800fe97ddf464e3f2b2f8dbc796
Kane Baltazar Alanoca <developer.innovaappstar@gmail.com>
2024-06-09 10:21:11 -0500

    first commit

---
+++ app/src/main/java/com/upn/garage/ui/FirstActivity.kt
@@ -0,0 +1,14 @@
+package com.upn.garage.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.upn.garage.R
+
+class FirstActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+
+    }
+}
